bec69e0d55206be16f1c26d196604a9dbc94cb6d
Dhruv <dshivhare661@gmail.com>
2024-11-20 12:30:46 +0530

    sec

--- index.js
+++ index.js
@@ -1,19 +1,36 @@
  async function fet(){
     let a = await fetch("http://localhost:3000/students")
     let b = await a.json()
-    let final_data = b.map((t)=>`
+   //  let final_data = b.map((t)=>`
 
-    <h1> ${t.id} </h1>
-    <h1> ${t.name} </h1>
-    <h1> ${t.age} </h1>
-    <h1> ${t.add} </h1>
+   //  <h1> ${t.id} </h1>
+   //  <h1> ${t.name} </h1>
+   //  <h1> ${t.age} </h1>
+   //  <h1> ${t.add} </h1>    
     
     
     
-    `).join("")
+   //  `).join("")
 
-    document.querySelector('#showdata').innerHTML = final_data
+   let final_data = b.map((t)=>`
+   <tr>
+   <td> ${t.id} </td>
+   <td> ${t.name} </td>
+   <td> ${t.age} </td>
+   <td> ${t.add} </td>
+   <td> <button onclick="mydelete('${t.id}')">Delete </button> </td>
+   </tr>
+   `).join("  ")
+   
+   document.querySelector('#anshul').innerHTML = final_data
 
  }
 
- fet()
+ fet();
+
+ function mydelete(id){
+   fetch(`http://localhost:3000/students/${id}`,{
+      method:'DELETE'
+   })
+   .then( re=>alert("deleted successfully..!!!"))
+ }
