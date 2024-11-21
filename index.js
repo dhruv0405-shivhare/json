eb8bb9d919f849e0f9a2966546ccedd4665b8342
Dhruv <dshivhare661@gmail.com>
2024-11-21 12:20:47 +0530

    insert data

--- index.js
+++ index.js
@@ -33,4 +33,30 @@
       method:'DELETE'
    })
    .then( re=>alert("deleted successfully..!!!"))
+
  }
+
+
+
+//insert data
+
+function insertdata(){
+  let data = {
+    name:document.querySelector('#name').value,
+    age:document.querySelector('#age').value,
+    address:document.querySelector('#address').value
+  }
+
+
+
+
+fetch("http://localhost:3000/students",{
+  method:'POST',
+  headers:{
+    'Content-type':'applicaion/json'
+  },
+  body:JSON.stringify(data)
+}) 
+.then(res=>alert("inserted..."))
+
+}
